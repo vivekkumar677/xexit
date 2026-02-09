fa2a12dce44e853400045284c63d03c318f05d37
Vivek Kumar <ervivekkumar677@gmail.com>
2026-02-10 03:51:07 +0530

    server.js updated

--- assessment/server.js
+++ assessment/server.js
@@ -25,6 +25,4 @@ app.get("/", (req, res) => {
     res.send("Welcome to the ME_MERN_XEXIT API");
 });
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-});
+export default app;
